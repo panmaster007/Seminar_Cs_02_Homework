6a536ab70bd1c8a0560b94dc12a865fd1f706f8b
Igor <fullstack@naladka.su>
2023-02-05 22:50:15 +0300

    edit task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Введите число: ");
+
+if(int.TryParse(Console.ReadLine(), out int number))
+{
+   if (number > 99)
+	{
+		while (number > 999)
+		{
+			number = number / 10;
+		}
+		number = number % 10;
+		System.Console.WriteLine($"Третяя цифра введённого числа {number}");
+   }
+   else System.Console.WriteLine("Введенное число является менее чем трехзначным и третьей цифры у него нет");
+}
+else System.Console.WriteLine("Вы ввели не целое число или вовсе не число");
