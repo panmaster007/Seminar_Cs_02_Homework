7b1428039fc29f2cf848ec8218deb2ae1d192aef
Igor <fullstack@naladka.su>
2023-02-05 22:19:29 +0300

    edit task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите число: ");
+if(int.TryParse(Console.ReadLine(), out int number))
+{
+	number = number / 10;
+	number = number % 10;
+	Console.WriteLine($"Вторая цифра введённого числа {number}");
+}
+else System.Console.WriteLine("Вы ввели не целое число или вовсе не число");
